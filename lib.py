9254572d3292aa9b4fdbf0256d751e58bc3ce3f7
張天慈 <woodychang0611@gmail.com>
2020-12-15 23:42:20 +0800

    update code

--- lib.py
+++ lib.py
@@ -41,3 +41,27 @@ def set_matplotlib_style():
             matplotlib.rcParams[name] ='#B9CAFF'
         if matplotlib.rcParams[name]=='white':
             matplotlib.rcParams[name] ='#0C0C3A'    
+
+def read_investments(sd=datetime(1980,1,1),ed = datetime.today()):
+    investments_dataframe= pd.DataFrame(index=pd.date_range(start=sd,end=ed, freq='D'))
+    investments ={
+        "Bitcoin":"BTC-USD",    
+        "Tesla":"TSLA",    
+        "S&P500":"VFINX",
+        "Bond":"VBMFX",
+        "Gold": "GC=F",
+    }
+
+    investments_data = "./data/investments_data.csv"
+    s=[]
+    if (os.path.exists(investments_data)):
+        print("Load existing data")
+        investments_dataframe = pd.read_csv(investments_data, parse_dates=True,index_col='Date')
+    else:
+        for name in investments.keys():
+            s = (pdr.get_data_yahoo(symbols=investments[name], start=sd, end=ed)["Adj Close"]).rename(name)
+            #drop duplicated index
+            s = s.groupby(level=0).last()
+            investments_dataframe[name]=s
+        investments_dataframe.to_csv(path_or_buf=investments_data,index_label='Date')
+    return investments_dataframe
