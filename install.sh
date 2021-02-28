f93a7568b4a0465c7dd9e6941a438fd87c87a7eb
Woody Chang <woodychang0611@gmail.com>
2021-02-28 14:09:06 +0000

    test

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-apt-get update 
-apt-get install libopenmpi-dev
+apt-get update
 pip install numpy==1.18.5
 pip install  --upgrade  tqdm
+apt-get install libopenmpi-dev
+
 rm -rf /workspace/spinningup
 cd /workspace
 git clone https://github.com/openai/spinningup.git
