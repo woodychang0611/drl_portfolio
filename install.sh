11fe20aa9cc7cb22d5165b00606e809de9cd519d
Woody Chang <woodychang0611@gmail.com>
2021-03-01 09:59:16 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@ apt-get update
 
 git config --global user.email "woodychang0611@gmail.com"
 git config --global user.name "Woody Chang"
-pip install numpy==1.18.5
-pip install  --upgrade  tqdm
-apt-get install libopenmpi-dev
 
-rm -rf /workspace/spinningup
 cd /workspace
-git clone https://github.com/openai/spinningup.git
-cd spinningup
-pip install -e .
+#git clone https://github.com/openai/spinningup.git
+#cd spinningup
+#pip install -e .
 
