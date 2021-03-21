dff71b5a5e84ff566d316a0f804f728189fdb36e
張天慈 <woodychang0611@gmail.com>
2021-03-21 23:39:51 +0800

    update

--- investments_analysis.py
+++ investments_analysis.py
@@ -8,7 +8,7 @@ import math
 current_folder = os.path.dirname(__file__)
 
 
-investments_return_csv = os.path.join(
+investments_returns_csv = os.path.join(
     current_folder, './data/investments_67ETF.csv')
 investments_summary = os.path.join(
     current_folder, './data/investments_summary.csv')
@@ -16,12 +16,29 @@ output_folder = os.path.join(
     current_folder, f'./output/investments_{timestamp()}')
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
-investments_return = pd.read_csv(investments_return_csv, parse_dates=[
-                                 'Date'], index_col=['Date'])
-investments_return = investments_return.sort_index()
-investments_price = pd.DataFrame(index=investments_return.index)
+investments_returns = pd.read_csv(investments_returns_csv, parse_dates=[
+    'Date'], index_col=['Date'])
+investments_returns = investments_returns.sort_index()
+investments_prices = pd.DataFrame(index=investments_returns.index)
+investments_returns_ma = pd.DataFrame(index=investments_returns.index)
 investments_summary = pd.read_csv(investments_summary, index_col=0)
-investment_names = investments_return.columns[:]
+investment_names = investments_returns.columns[:]
+
+
+def average_return(s):
+    return math.pow(np.prod(s+1), 1.0/len(s))-1
+
+
+for name in investment_names:
+    investments_prices[name] = finance_utility.prices_from_returns(
+        1, investments_returns[name])
+    investments_returns_ma[name] = investments_returns[name].rolling(
+        60).apply(func=average_return, raw=False)
+
+
+corr_df = investments_returns_ma.corr()
+corr_df.to_csv(os.path.join(output_folder, 'corr.csv'))
+
 
 investments_summary['mdd'] = pd.Series(dtype='float64')
 investments_summary['std'] = pd.Series(dtype='float64')
@@ -29,39 +46,39 @@ investments_summary['cagr'] = pd.Series(dtype='float64')
 investments_summary['sharpe'] = pd.Series(dtype='float64')
 
 for name in investment_names:
-    prices = finance_utility.prices_from_returns(1, investments_return[name])
-    std = investments_return[name].std()*math.sqrt(252)
-    duration = (investments_return[name].index[-1]-investments_return[name].index[0]).days
+    prices = finance_utility.prices_from_returns(1, investments_returns[name])
+    #Wiener process
+    std = investments_returns[name].std()*math.sqrt(252)
+    duration = (investments_returns[name].index[-1] -
+                investments_returns[name].index[0]).days
     cagr_value = finance_utility.cagr(prices[0], prices[-1], duration)
-    sharpe_ratio = cagr_value/std
-    mdd=finance_utility.drawdown(prices)
-    investments_summary.loc[name, 'mdd']=mdd
-    investments_summary.loc[name, 'std']=std
-    investments_summary.loc[name, 'cagr']=cagr_value
-    investments_summary.loc[name, 'sharpe_ratio']=sharpe_ratio
+    sharpe = cagr_value/std
+    mdd = finance_utility.drawdown(prices)
+    investments_summary.loc[name, 'mdd'] = mdd
+    investments_summary.loc[name, 'std'] = std
+    investments_summary.loc[name, 'cagr'] = cagr_value
+    investments_summary.loc[name, 'sharpe'] = sharpe
 # Start from all investments
-selected_investment=set(investment_names)
-corr_df=investments_return.corr()
-corr_df.to_csv(os.path.join(output_folder, 'corr.csv'))
+selected_investment = set(investment_names)
 
-selection_count=300
-keep_investment=('SPY', 'QQQ')
-keep_investment=('SPY')
-while len(selected_investment) > 10:
-    max_corr={}
+
+while len(selected_investment) > 30:
+    max_corr = {}
+    def convert_one_to_nan(s): return s if (s != 1) else np.nan
     for investment in selected_investment:
-        if (investment in keep_investment):
-            continue
-        max_corr[investment]=max(corr_df[investment], key = (
-            lambda s: s if (s != 1) else np.nan))
+        max_corr[investment] = np.nanmax(
+            corr_df[investment].map(convert_one_to_nan))
 
-    key, max_value = find_dict_max(max_corr)
-    if (max_value <= 0.99999):
-        break
-    corr_df = corr_df.drop(key, axis=0)
-    corr_df = corr_df.drop(key, axis=1)
-    print(f'drop {key} {max_value}')
-    selected_investment.remove(key)
+    # Keep the one with highest AUM (Asset Under Management)
+    s = investments_summary['AUM']
+    def compare_func(k): return ((max_corr[k], s[k]))
+    drop_name = sorted(selected_investment, key=compare_func)[-2]
+    keep_name = sorted(selected_investment, key=compare_func)[-1]
+    print(
+        f'drop {drop_name} {compare_func(drop_name)}, keep {keep_name} {compare_func(keep_name)}')
+    corr_df = corr_df.drop(drop_name, axis=0)
+    corr_df = corr_df.drop(drop_name, axis=1)
+    selected_investment.remove(drop_name)
 
 print(f'selected_investment:{selected_investment}')
 print(len(corr_df.columns))
