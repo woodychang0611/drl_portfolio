fcd5c978c4cf565b8721f0327c6e772a568f0a18
張天慈 <woodychang0611@gmail.com>
2021-04-04 23:55:04 +0800

    update

--- common/market_env.py
+++ common/market_env.py
@@ -11,16 +11,23 @@ from pandas import DataFrame
 
 
 class MarketEnv(gym.Env):
+    def __init__(self, investments: DataFrame, features: DataFrame, show_info=False, trade_freq='days',
+                 fix_start_time=False, min_trade_pecentage=0.1):
+        self._load_data(investments=investments, features=features, show_info=show_info, trade_freq=trade_freq)
+        self._init_action_space()
+        self._init_observation_space()
+        self.seed()
+        self.reset()
 
-    def load_data(self, investments: DataFrame, features: DataFrame, show_info, trade_freq):
-        sample_rules = {
+    def _load_data(self, investments: DataFrame, features: DataFrame, show_info, trade_freq):
+        resample_rules = {
             'days': 'D',
-                    'weeks': 'W',
-                    'months': 'M'
+            'weeks': 'W',
+            'months': 'M'
         }
 
-        if(trade_freq not in sample_rules.keys()):
-            raise ValueError(f"trade_freq '{trade_freq}' not supported, must be one of {list(sample_rules.keys())}")
+        if(trade_freq not in resample_rules.keys()):
+            raise ValueError(f"trade_freq '{trade_freq}' not supported, must be one of {list(resample_rules.keys())}")
 
         if investments.isnull().values.any():
             raise ValueError('At least one null value in investments')
@@ -33,8 +40,8 @@ class MarketEnv(gym.Env):
         features = features[(features.index.isin(investments.index))]
 
         # resample based on trade frequency e.g. weeks or months
-        investments = investments.apply(lambda x: 1+x).resample(sample_rules[trade_freq]).prod().apply(lambda x: x-1)
-        features = features.apply(lambda x: 1+x).resample(sample_rules[trade_freq]).prod().apply(lambda x: x-1)
+        investments = investments.apply(lambda x: 1+x).resample(resample_rules[trade_freq]).prod().apply(lambda x: x-1)
+        features = features.apply(lambda x: 1+x).resample(resample_rules[trade_freq]).prod().apply(lambda x: x-1)
         # Scale to -1 and 1
         for col in features.columns:
             features[col] = features[col]/max(abs(features[col].max()), abs(features[col].min()))
@@ -49,25 +56,17 @@ class MarketEnv(gym.Env):
             print(f'{len(self.features.columns)} features loaded')
             print(f'Starts from {sd} to {ed}')
 
-    def init_action_space(self):
+    def _init_action_space(self):
         action_space_size = len(self.investments.columns)
         self.min_action = np.zeros(action_space_size)
         self.max_action = np.ones(action_space_size)
         self.action_space = spaces.Box(low=self.min_action, high=self.max_action, dtype=np.float32)
 
-    def init_observation_space(self):
+    def _init_observation_space(self):
         observation_space_size = len(self.investments.columns) + len(self.features.columns)
         self.low_state = np.full(observation_space_size, -1)
         self.high_state = np.ones(observation_space_size)
         self.observation_space = spaces.Box(low=self.low_state, high=self.high_state, dtype=np.float32)
-
-    def __init__(self, investments: DataFrame, features: DataFrame, show_info=False, trade_freq='days',
-                 fix_start_time=False, min_trade_pecentage=0.1):
-        self.load_data(investments=investments, features=features, show_info=show_info, trade_freq=trade_freq)
-        self.init_action_space()
-        self.init_observation_space()
-        self.seed()
-        self.reset()
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
--- rlkit_test/test.py
+++ rlkit_test/test.py
@@ -1,16 +1,7 @@
 import numpy as np
 from math import dist
-from my_gym_env import MyGymEnv
 from random import uniform
 import pandas as pd
 
-def test():
-    n1 = 1
-    def test2():
-        nonlocal  n1
-        print(n1)
-        n1 = 123
-    test2()
-    print(n1)
-
-test()
+a = np.full(5,1)
+print(a)
