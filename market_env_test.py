2c2d648341e5d1da7c509edb766730ebba5aaa25
張天慈 <woodychang0611@gmail.com>
2021-05-04 07:14:58 +0800

    update test

--- market_env_test.py
+++ market_env_test.py
@@ -1,51 +1,41 @@
+import os
 import gym
-from rlkit.envs.wrappers import NormalizedBoxEnv
-from rlkit.torch.sac.policies import MakeDeterministic
-from rlkit.samplers.data_collector import MdpPathCollector
-from rlkit.launchers.launcher_util import setup_logger
-from rlkit.data_management.env_replay_buffer import EnvReplayBuffer
 import pandas as pd
 from pandas import Timestamp
-import os
+import numpy as np
+from datetime import datetime
+import torch
+import json
+import colorsys
+
 import common
 from common.trainer import get_trainer
 from common.market_env import MarketEnv
-import rlkit.torch.pytorch_util as ptu
-import numpy as np
-from datetime import datetime
-from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 from common.finance_utility import finance_utility
-import torch
-import json
 from common.market_env import simple_return_reward, sharpe_ratio_reward
+
+from rlkit.envs.wrappers import NormalizedBoxEnv
+from rlkit.torch.sac.policies import MakeDeterministic
+import rlkit.torch.pytorch_util as ptu
+
 
 def get_unwrapped_env(env):
     return env._wrapped_env.unwrapped
 
-def eval_policy(env, policy, df=None):
+def eval_policy(env, policy):
     done = False
     state = env.reset()
     rewards =[]
     weights=[]
+    infos=[]
     while not done:
         action = policy.get_actions(state)
         state, reward, done, info = env.step(action)
         weights.append((get_unwrapped_env(env)).weights)
-        #print(f'state: {state}')    
-        #print(f'action: {action}')
-        #print(f'reward: {reward}')
         rewards.append(reward)
-        print(info)
-        if (df is not None):
-            df = df.append(info, ignore_index=True)
+        infos.append(info)
     rewards = np.array(rewards)
-    print(f'mean:{np.mean(rewards)}')
-    print(f'std:{np.std(rewards)}')
-    print(f'ratio:{np.mean(rewards)/np.std(rewards)}')
-    weights_df = pd.DataFrame(weights)
-    weights_df.to_csv('action')
-    if (df is not None):
-        return df
+    return pd.DataFrame(weights),pd.DataFrame(infos)
 
 def fix_action_policy(action):
     class dummy_policy(object):
@@ -55,9 +45,10 @@ def fix_action_policy(action):
 
 
 
-
+ptu.set_gpu_mode(True)
 
 src = r'C:\Users\Woody\Documents\git repository\nccu-thesis\code\output\train_out_20210502_230851'
+src=r'C:\Users\Woody\Documents\git repository\nccu-thesis\code\trained\train_out_20210502_234701'
 with open(os.path.join(src,'variant.json')) as json_file:
     variant=json.load(json_file)
 
@@ -84,20 +75,27 @@ eval_env = NormalizedBoxEnv(gym.make('MarketEnv-v0', returns=df_ret_val, feature
 env = eval_env
 trainer = get_trainer(env=env,**trainer_kwargs)
 file =os.path.join(src,'params.pkl')
-v = torch.load(file)
-print(type(trainer.policy))
-print(v['evaluation/policy'])
-ptu.set_gpu_mode(True)
+
 trainer.policy =  torch.load(file)['evaluation/policy']
 policy = MakeDeterministic(trainer.policy)
 
+
+
+
+weights,infos = eval_policy(env,policy)
+weights.to_csv('weights.csv')
+print(infos)
+import matplotlib.pyplot as plt
+
+n = len(weights.columns)
+col=map(lambda s:colorsys.hls_to_rgb(0.7, 0.8,1-0.5*float(s)/n),range(n))
+#print(list(col))
+#col= [colorsys.hls_to_rgb(0.5, 1, 0.5), "#e74c3c", "#34495e", "#2ecc71"]
+plt.stackplot(weights.index,weights.T,colors=col)
+plt.show()
 #action = np.full(10,-1)
 #action[5]=1
 #policy = fix_action_policy(action)
-
-df = pd.DataFrame()
-df = eval_policy(env,policy,df)
-df.to_csv('test.csv')
 
 
 
